4f1b25411581efb0ebc16874cdf798f1bb83a6c5
AndreFerreira20 <andre.ferreira20@unifesp.br>
2026-05-13 01:46:23 -0300

    Criacao do padrao Factory

---
+++ src/Reserva-Salas-Estudo/SalaFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva_Salas_Estudo
+{
+    internal class SalaFactory
+    {
+        public static Sala CriarSala(string tipo, string cod, int cap)
+        {
+            switch (tipo.ToLower())
+            {
+                case "laboratorio":
+                    return new Laboratorio(cod, cap);
+                case "individual":
+                    return new SalaIndividual(cod, 1);
+                case "grupo":
+                    return new SalaGrupo(cod, cap);
+                default:
+                    throw new ArgumentException("não existe esse tipo de sala");
+
+            }
+        }
+    }
+}
