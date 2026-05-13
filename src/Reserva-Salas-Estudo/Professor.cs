74f1f9c44da5f6b67ad255c5cb18d6b53fcc912e
AndreFerreira20 <andre.ferreira20@unifesp.br>
2026-05-13 01:12:32 -0300

    Estrutura sem acoplamento dos usuarios

---
+++ src/Reserva-Salas-Estudo/Professor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva_Salas_Estudo
+{
+    public class Professor : Usuario
+    {
+        private string siape;
+
+        public Professor(string nome, string email, string tipo, string siape) : base(nome, email, tipo)
+        {
+            this.siape = siape;
+        }
+
+        public string GetSiape() { return siape; }
+    }
+}
---
+++ src/Reserva-Salas-Estudo/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Reserva_Salas_Estudo
+{
+    public abstract class Usuario 
+    {
+        private Guid id = Guid.NewGuid();
+        private string nome;
+        private string email;
+        private string tipoUsuario;
+
+        protected Usuario(string nome, string email, string tipo)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.tipoUsuario = tipo;
+        }
+
+        public void Update(string mensagem)
+        {
+            Console.WriteLine($"[Notificação para {GetNome()}]: {mensagem}");
+        }
+
+
+        // Sets e Gets
+        public void SetNome(string nome) { this.nome = nome; }
+        public void SetEmail(string email) { this.email = email; }
+        public void SetTipoUsuario(string tipo) { this.tipoUsuario = tipo; }
+
+        public string GetNome() { return nome; }
+        public string GetEmail() { return email; }
+        public string GetTipoUsuario() { return tipoUsuario; }
+    }
+}
