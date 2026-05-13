c7d026ffaaf2ed560551001e2b6de3c14a4c6e9f
AndreFerreira20 <andre.ferreira20@unifesp.br>
2026-05-13 01:05:03 -0300

    Estrutura inicial do Singleton repositorio de reservas

---
+++ src/Reserva-Salas-Estudo/RepositorioReservas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva_Salas_Estudo
+{
+    public class RepositorioReservas
+    {
+        private static RepositorioReservas instance;
+        //private static List<Reserva> a;
+
+        public static RepositorioReservas GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RepositorioReservas();
+            }
+
+            return instance;
+        }
+
+        private RepositorioReservas() { }
+    }
+}
