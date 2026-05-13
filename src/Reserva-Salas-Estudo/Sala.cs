6ed11b06afeb2525d2770df8762f747da410b0b2
AndreFerreira20 <andre.ferreira20@unifesp.br>
2026-05-13 01:03:05 -0300

    Criacao do conceito inicial das salas

---
+++ src/Reserva-Salas-Estudo/Sala.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva_Salas_Estudo
+{
+    public abstract class Sala
+    {
+        private string codigo;
+        private int capacidade;
+        private List<string> recursos;
+
+        public abstract bool Disponivel(int inicio, int fim);
+
+        public void SetCodigo(string codigo) { this.codigo = codigo; }
+        public void SetCapacidade(int capacidade) { this.capacidade = capacidade; }
+        public void SetRecursos(List<string> recursos) { this.recursos = recursos; }
+    }
+}
---
+++ src/Reserva-Salas-Estudo/Salas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reserva_Salas_Estudo
+{
+    // Sala para estudo individual. 
+    public class SalaIndividual : Sala
+    {
+        private bool silenciosa;
+        private int mesas;
+
+        public SalaIndividual(string codigo, int capacidade, List<string> recursos, bool silenciosa, int mesas)
+        {
+            SetCodigo(codigo);
+            SetCapacidade(capacidade);
+            SetRecursos(recursos);
+
+            this.silenciosa = silenciosa;
+            this.mesas = mesas;
+        }
+
+        public override bool Disponivel(int inicio, int fim)
+        {
+            //    return true;
+        }
+    }
+
+    // Sala para estudo com computadores.
+    public class Laboratorio : Sala
+    {
+        private int computadores;
+        private List<string> softwares;
+
+        public Laboratorio(string codigo, int capacidade, List<string> recursos, int computadores, List<string> softwares)
+        {
+            SetCodigo(codigo);
+            SetCapacidade(capacidade);
+            SetRecursos(recursos);
+
+            this.computadores = computadores;
+            this.softwares = softwares;
+        }
+
+        public override bool Disponivel(int inicio, int fim)
+        {
+            //    return true;
+        }
+    }
+
+    // Sala para estudos em grupo.
+    internal class SalaGrupo : Sala
+    {
+        private bool quadro;
+        private int maxGrupos;
+
+        public SalaGrupo(string codigo, int capacidade, List<string> recursos, bool quadro, int maxGrupos)
+        {
+            SetCodigo(codigo);
+            SetCapacidade(capacidade);
+            SetRecursos(recursos);
+
+            this.quadro = quadro;
+            this.maxGrupos = maxGrupos;
+        }
+
+        public override bool Disponivel(int inicio, int fim)
+        {
+            //    return true;
+        }
+    }
+}
